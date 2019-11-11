579cb9c144e6d122ecbdea7ed95bf74865c539a6
AmrutheshwarO <55506973+AmrutheshwarO@users.noreply.github.com>
2019-11-11 16:29:49 +0530

    Add files via upload

---
+++ cq.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define size 5
+int cq[size], front=-1, rear=-1, item;
+void insert(){
+	if(rear==(front-1)||(front==0&&rear==(size-1)))
+		printf("Overflow\n");
+	else{
+		printf("Enter the element.\n");
+		scanf("%d", &item);
+		if(front==-1&&rear==-1)
+			front=0;
+		rear=(rear+1)%size;
+		cq[rear]=item;
+	}
+}
+void delete(){
+	if(front==-1&&rear==-1)
+		printf("Underflow.\n");
+	else{
+		if(front==rear){
+			front=-1;
+			rear=-1;
+		}
+		else
+			front=(front+1)%size;
+	}
+}
+void display(){
+	if(rear=-1&&front==-1)
+		printf("Circular queue is empty.\n");
+	else{
+		int i=0;
+		for(i=0; i!=rear; i=(i+1)%size)
+			printf("%d--", cq[i]);
+		printf("%d--", cq[i]);
+	}		
+}
+void main(){
+	int ch;
+	while(1){
+		printf("Enter your choice.\n");
+		printf("1. Insert\n2. Delete\n3. Display\n4. Exit\n");
+		scanf("%d", &ch);
+		switch(ch){
+			case 1: insert();
+				break;
+			case 2: delete();
+				break;
+			case 3: display();
+				break;
+			case 4: exit(0);
+			default: printf("Invalid choice\n");
+				 break;
+		}
+	}
+}
+	
+		
